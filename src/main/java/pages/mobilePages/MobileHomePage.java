6d149da39ca1a1b604953bbc316dad394e2692c5
ShreyaKhosla <khosla.shreya08@gmail.com>
2016-11-08 10:29:44 +0530

    Login updated

--- src/main/java/pages/mobilePages/MobileHomePage.java
+++ src/main/java/pages/mobilePages/MobileHomePage.java
@@ -2,13 +2,10 @@ package pages.mobilePages;
 
 import org.openqa.selenium.ElementNotVisibleException;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 
-import objectRepository.HomePageLocator;
 import objectRepository.LoginPageLocators;
 import pages.BasePage;
 import utils.CommonFunctionLibrary;
-import utils.ConfigManager;
 
 public class MobileHomePage extends BasePage {
 
